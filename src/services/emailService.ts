7e287faabc43c80d787640a9f306407831d9d945
Julian Basilieri <julian.basilieri@darwoft.com>
2025-10-23 12:05:17 -0300

    agregar QR a mail

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -1,5 +1,6 @@
 import emailjs from '@emailjs/browser';
 import { createDateFromStr } from '../utils/dateUtils';
+import QRCode from 'qrcode';
 
 const ENV = import.meta.env
 
@@ -14,6 +15,29 @@ interface EmailData {
     reservationCode?: string;
 }
 
+// Función para generar el código QR como Data URI
+const generateQrCode = async (data: EmailData): Promise<string> => {
+    try {
+        // Crear objeto con la información relevante para el QR
+        const qrData = {
+            email: data.email,
+            transactionId: data.transactionId,
+            visitDate: data.visitDate,
+            ticketCount: data.ticketCount,
+            ticketType: data.ticketType,
+            totalAmount: data.totalAmount,
+            reservationCode: data.reservationCode || '',
+            paymentMethod: data.paymentMethod
+        };
+
+        // Generar y retornar el QR como data URI
+        return await QRCode.toDataURL(JSON.stringify(qrData));
+    } catch (error) {
+        console.error('Error generando código QR:', error);
+        return '';
+    }
+};
+
 export const emailService = {
     sendPurchaseConfirmation: async (data: EmailData) => {
         try {
@@ -27,6 +51,9 @@ export const emailService = {
                 console.error('Faltan variables de entorno para EmailJS');
             }
 
+            // Generar código QR
+            const qrCodeDataUri = await generateQrCode(data);
+
             // Parámetros base comunes
             const baseParams = {
                 email: data.email,
@@ -38,6 +65,7 @@ export const emailService = {
                 ticketType: data.ticketType.toUpperCase(),
                 totalAmount: data.totalAmount.toLocaleString('es-AR'),
                 reservationCode: data.reservationCode,
+                qrCodeImage: qrCodeDataUri, // Incluir la imagen QR
             };
 
             // Selección del template y parámetros específicos
@@ -56,12 +84,18 @@ export const emailService = {
                 selectedTemplate = ENV.VITE_EMAILJS_TEMPLATE_ID_MERCADOPAGO;
             }
 
-            await emailjs.send(
-                ENV.VITE_EMAILJS_SERVICE_ID,
-                selectedTemplate,
-                templateParams,
-                ENV.VITE_EMAILJS_PUBLIC_KEY
-            );
+            console.log(qrCodeDataUri)
+
+            console.log(ENV.VITE_ENVIO_MAIL)
+
+            if (ENV.VITE_ENVIO_MAIL) {
+                await emailjs.send(
+                    ENV.VITE_EMAILJS_SERVICE_ID,
+                    selectedTemplate,
+                    templateParams,
+                    ENV.VITE_EMAILJS_PUBLIC_KEY
+                );
+            }
 
             return { success: true };
         } catch (error) {
